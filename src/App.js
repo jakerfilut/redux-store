c7b4d91b58c40470bd1ef5dbc5bbe93c830078f7
Jake <jakerfilut@gmail.com>
2022-03-08 16:24:06 -0700

    redux test done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import Header from "./containers/Header";
 import "./App.css";
+import ProductDetails from "./containers/ProductDetails";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
+      <Routes>
+        {/* <Switch> */}
+        <Route path="/" exact component={ProductListing} />
+        <Route path="/product/:productId" component={ProductDetails} />
+        <Route>404 Not Found!</Route>
+        {/* </Switch> */}
+      </Routes>
     </div>
   );
 }
